35784a22fdcf4466ac884946947f10cd3ed1c562
Natalisa <nnsuhodoeva@mail.ru>
2017-06-04 15:08:29 +0700

    Переработана форма и работа с ботами

--- tg.py
+++ tg.py
@@ -26,13 +26,20 @@ def comands(message):
 def Tg_mes_text_send(mes,sdvig):
     return  sdvig*' '+'''bot.send_message(message.chat.id, \'\'\'''' + str(mes)+ '''\'\'\')
 '''
+
 def Tg_mes_text_if(mas,otv,sdvig):
     log = ''
     for item,item2 in zip(mas,otv):
-        log += sdvig*' '+'''if message.text == ''' + '''\''''+ item + '''\'''' + ''':
+        if item2[0] == '$':
+            item2 = item2[1:]
+            if ':' in item2:
+                M = (item2.split(':'))[0]
+                A = (item2.split(':'))[1]
+                log += sdvig*' '+'''if message.text == "''' + item + '''":\n'''+ Tg_mes_text_send('''getattr(sys.modules["''' + M + '''"], "todo")("''' + A + '''")''',sdvig+2)
+        else:
+            log += sdvig*' '+'''if message.text == ''' + '''\''''+ item + '''\'''' + ''':
 ''' +Tg_mes_text_send(item2,sdvig+2) +'\n'
     return log
-
 
 # if __name__ == '__main__':
 #     bot.polling(none_stop=True)
